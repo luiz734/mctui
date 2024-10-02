4b2da0fca884bf68f660c957a2f2622af304818b
Luiz G. <luiz007@live.com>
2024-10-01 23:29:23 -0300

    Add rcon command view

--- app/command.go
+++ app/command.go
@@ -11,11 +11,24 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-type rconCommand string
-type rconOutput string
 type rconEntry struct {
-	command rconCommand
-	output  rconOutput
+	command string
+	output  string
+}
+
+func (e *rconEntry) View() string {
+	commandStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color(colors.Pink)).
+		Bold(true)
+	commandStr := commandStyle.Render(e.command)
+
+	outputStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color(colors.Surface2))
+	outputStr := outputStyle.Render(e.output)
+
+	both := fmt.Sprintf("%s\n%s\n", commandStr, outputStr)
+	return both
+
 }
 
 type commandModel struct {
@@ -26,7 +39,10 @@ type commandModel struct {
 	err          error
 }
 
-type outputMsg rconCommand
+type outputMsg struct {
+	c string
+	o string
+}
 
 func InitialCommandModel() commandModel {
 	ci := textinput.New()
@@ -57,7 +73,7 @@ func (m commandModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case tea.KeyCtrlC:
 			return m, tea.Quit
 		case tea.KeyEnter:
-			cmd = sendCommand(rconCommand(m.commandInput.Value()))
+			cmd = sendCommand(m.commandInput.Value())
 			m.commandInput.SetValue("")
 			return m, cmd
 		}
@@ -68,10 +84,10 @@ func (m commandModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.err = msg
 		return m, nil
 	case outputMsg:
-        // e := rconEntry {
-        //     rconCommand:
-        // }
-		m.history = append(m.history, rconEntry{})
+		m.history = append(m.history, rconEntry{
+			command: msg.c,
+			output:  msg.o,
+		})
 		return m, nil
 
 	case tea.WindowSizeMsg:
@@ -92,8 +108,9 @@ func (m commandModel) View() string {
 
 	var lines strings.Builder
 	for _, command := range m.history {
-		line := fmt.Sprintf("##%s##\n", command.command)
+		line := command.View()
 		lines.WriteString(line)
+        lines.WriteString("\n")
 	}
 	lines.WriteString("\n")
 
@@ -121,8 +138,8 @@ func (m commandModel) View() string {
 	return fmt.Sprintf("%s\n", both)
 }
 
-func sendCommand(command rconCommand) tea.Cmd {
+func sendCommand(command string) tea.Cmd {
 	return func() tea.Msg {
-		return outputMsg(command)
+		return outputMsg{command, "this is output"}
 	}
 }
